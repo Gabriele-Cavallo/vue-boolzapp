22a7afd620a6a16f31018a8b3d8b6de91928ad75
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-04-03 17:01:32 +0200

    feat: add dynamic chat

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@
 // Milestone 2
 
 // - Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi 
-//relativi al contatto attivo all’interno del pannello della conversazione
+//   relativi al contatto attivo all’interno del pannello della conversazione
 // - Click sul contatto mostra la conversazione del contatto cliccato
 const { createApp } = Vue;
 
